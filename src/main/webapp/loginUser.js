2031f92799d95b799e9138ca5f693232f8b6e828
Allison Lim <aklim@usc.edu>
2024-04-26 01:02:57 -0700

    login js

--- src/main/webapp/loginUser.js
+++ src/main/webapp/loginUser.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
     const loginButton = document.getElementById("loginButton");
     const newUserButton = document.getElementById("newUserButton");
+    const submitButton = document.getElementById("submit-login");
     var email = document.getElementById("email");
     var password = document.getElementById("password");
     var passwordRepeat = document.getElementById("password-repeat");
@@ -13,5 +14,27 @@ document.addEventListener("DOMContentLoaded", function(){
 
     newUserButton.addEventListener("click", function(){
         window.location.href = "createUser.html";
+    })
+    
+   	submitButton.addEventListener("click", function(){
+        var xhr = new XMLHttpRequest();
+		var url = "LoginServlet";
+		var encodeEmail = encodeURIComponent(email.value);
+		var encodePassword = encodeURIComponent(password.value);
+		var params = "email=" + encodeEmail + "&password=" + encodePassword;
+	
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == XMLHttpRequest.DONE) {
+				if (xhr.status == 200) {
+					//valid response
+					window.location.href = "loginUser.html";
+				} else {
+					alert("Error: " + xhr.status);
+				}
+			}
+		};
+		xhr.send(params);
     })
 });
