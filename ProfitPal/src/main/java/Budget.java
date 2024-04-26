2606bc82416042f8e3fa1591eec6e0ecbe6aa25e
Sid Markurthi <markurth@usc.edu>
2024-04-25 22:56:16 -0700

    Create Budget.java

---
+++ ProfitPal/src/main/java/Budget.java
@@ -0,0 +1,41 @@
+public class Budget {
+
+private double grocery; //
+private double restaurant; //
+private double shopping; // 
+private double gas; //
+
+
+	public double getGrocery() {
+		return grocery;
+	}
+	
+	public void setGrocery(double c) {
+		this.grocery = c;
+	}
+	
+	public double getRestaurant() {
+		return restaurant;
+	}
+	
+	public void setRestaurant(double c) {
+		this.restaurant = c;
+	}
+	
+	public double getShopping() {
+		return shopping;
+	}
+	
+	public void setShopping(double c) {
+		this.shopping = c;
+	}
+	
+	public double getGas() {
+		return gas;
+	}
+	
+	public void setGas(double c) {
+		this.gas = c;
+	}
+
+}
