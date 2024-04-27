08606339c499fa45c00ff49ce6375e2fb351f67f
chasebak <chasebak@usc.edu>
2024-04-26 19:22:32 -0700

    Login JS Fixed

--- src/main/webapp/loginUser.js
+++ src/main/webapp/loginUser.js
@@ -12,29 +12,81 @@ document.addEventListener("DOMContentLoaded", function(){
 
     newUserButton.addEventListener("click", function(){
         window.location.href = "createUser.html";
-    })
-    
-   	submitButton.addEventListener("click", function(){
-        var xhr = new XMLHttpRequest();
-		var url = "LoginServlet";
-		var encodeEmail = encodeURIComponent(email.value);
-		var encodePassword = encodeURIComponent(password.value);
-		var params = "email=" + encodeEmail + "&password=" + encodePassword;
-	
-		xhr.open("POST", url, true);
-		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-		xhr.onreadystatechange = function() {
-			if (xhr.readyState == XMLHttpRequest.DONE) {
-				if (xhr.status == 200) {
-					//valid response
-					window.location.href = "loginUser.html";
-				} else {
-					alert("Error: " + xhr.status);
-				}
-			}
-		};
-		xhr.send(params);
     });
-    
-    
+
+	document.querySelector("#signup-form").onsubmit = function() {
+		
+		// Validate information for empty fields and rules before sending to the server
+		let validForm = true;
+
+		// Email validation
+		// Trim leading and trailed whitespaces.
+		const email = document.querySelector("#email").value.trim()
+
+		// Print to console
+		console.log(email)
+
+		// If string is empty
+		if (email.length === 0) {
+			validForm = false
+			// E1 - Note: Tag "#email-error" displays the email errors checked in JS -->
+			document.querySelector("#email-error").innerHTML = "Email cannot be empty."
+		}
+		// // Checks for usc.edu ending 
+		// else if (/usc\.edu$/.test(email) == false) {
+		//     validForm = false
+		//     document.querySelector("#email-error").innerHTML = 'Must be usc.edu email.'
+		// } 
+		// Checks for valid email with external JS
+		else if (!isEmailValid(email)) {
+			validForm = false 
+			document.querySelector("#email-error").innerHTML = 'Invalid Email'
+		} else {
+			// Else the email is correct, and clear the error message.
+			document.querySelector("#email-error").innerHTML = ''
+		}
+
+		// Password validation
+		const password = document.querySelector("#password").value;
+		console.log(password)
+		// Empty String
+		if (/^$/.test(password)) {
+			validForm = false
+			document.querySelector("#password-error").innerHTML = "Password cannot be empty."
+		} 
+		// Check security
+		else if (/(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*]).*/.test(password) == false) {
+			validForm = false
+			document.querySelector("#password-error").innerHTML = 'Insecure password'
+		} 
+		// valid
+		else {
+			document.querySelector("#password-error").innerHTML = ''
+		}
+		
+		console.log("is form valid: " + validForm);
+		// All checks passed then send to Servlet
+		if(validForm){
+			var xhr = new XMLHttpRequest();
+			var url = "LoginServlet";
+			var encodeEmail = encodeURIComponent(email.value);
+			var encodePassword = encodeURIComponent(password.value);
+			var params = "email=" + encodeEmail + "&password=" + encodePassword;
+		
+			xhr.open("POST", url, true);
+			xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+			xhr.onreadystatechange = function() {
+				if (xhr.readyState == XMLHttpRequest.DONE) {
+					if (xhr.status == 200) {
+						//valid response
+						window.location.href = "loginUser.html";
+					} else {
+						alert("Error: " + xhr.status);
+					}
+				}
+			};
+			xhr.send(params);
+		}
+		return false;
+    }
 });
