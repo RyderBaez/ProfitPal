c67a09d9cb5667dfffb3b16ca2c17e69c7b96249
chasebak <chasebak@usc.edu>
2024-04-29 20:22:06 -0700

    Previous Version w/o errors.

--- src/create.sql
+++ src/create.sql
@@ -4,22 +4,22 @@ CREATE DATABASE ProfitPal;
 
 USE ProfitPal;
 
-CREATE TABLE Users (
+CREATE TABLE ProfitPal.Users (
   userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
   email VARCHAR(45) NOT NULL,
   password VARCHAR(45) NOT NULL
 );
 
 
-CREATE TABLE BudgetItems (
+CREATE TABLE ProfitPal.BudgetItems (
     username VARCHAR(45) NOT NULL,
     category VARCHAR(45) NOT NULL,
     spending DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY (username, category)
 );
 
-INSERT INTO Users (password, email) VALUES ('test', 'test@gmail.com');
-INSERT INTO BudgetItems (username, category, spending) VALUES ('test', 'Grocery', 100.00);
-INSERT INTO BudgetItems (username, category, spending) VALUES ('test', 'Gas', 1000.00);
-INSERT INTO BudgetItems (username, category, spending) VALUES ('test', 'Other', 900.00);
-INSERT INTO BudgetItems (username, category, spending) VALUES ('test', 'Entertainment', 10.00);
+INSERT INTO ProfitPal.Users (password, email) VALUES ('test', 'test@gmail.com');
+INSERT INTO ProfitPal.BudgetItems (username, category, spending) VALUES ('test', 'Grocery', 100.00);
+INSERT INTO ProfitPal.BudgetItems (username, category, spending) VALUES ('test', 'Gas', 1000.00);
+INSERT INTO ProfitPal.BudgetItems (username, category, spending) VALUES ('test', 'Other', 900.00);
+INSERT INTO ProfitPal.BudgetItems (username, category, spending) VALUES ('test', 'Entertainment', 10.00);
