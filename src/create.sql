24442696e66c9a4103ced376d10e021079147461
chasebak <chasebak@usc.edu>
2024-04-29 17:56:06 -0700

    Updated SQL
    Fixed the previous SQL because it did not create a database and failed because of syntax and keywords.

--- src/create.sql
+++ src/create.sql
@@ -2,24 +2,24 @@ DROP DATABASE if exists ProfitPal;
 
 CREATE DATABASE ProfitPal;
 
-CREATE TABLE `201project`.`ProfitPal.Users` (
-  `email` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`email`));
-  ADD COLUMN `userID` INT NOT NULL AUTO_INCREMENT FIRST,
-  DROP PRIMARY KEY,
-  ADD PRIMARY KEY (`userID`);
+USE ProfitPal;
+
+CREATE TABLE Users (
+  userID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  email VARCHAR(45) NOT NULL,
+  password VARCHAR(45) NOT NULL
+);
 
 
-CREATE TABLE ProfitPal.BudgetItems (
+CREATE TABLE BudgetItems (
     username VARCHAR(45) NOT NULL,
     category VARCHAR(45) NOT NULL,
     spending DECIMAL(10, 2) NOT NULL,
     PRIMARY KEY (username, category)
 );
 
-INSERT INTO ProfitPal.Users (username, password, email) VALUES ('test', 'test', 'test@gmail.com');
-INSERT INTO ProfitPal.BudgetItems (username, category, spending) VALUES ('test', 'Grocery', 100.00);
-INSERT INTO ProfitPal.BudgetItems (username, category, spending) VALUES ('test', 'Gas', 1000.00);
-INSERT INTO ProfitPal.BudgetItems (username, category, spending) VALUES ('test', 'Other', 900.00);
-INSERT INTO ProfitPal.BudgetItems (username, category, spending) VALUES ('test', 'Entertainment', 10.00);
+INSERT INTO Users (password, email) VALUES ('test', 'test@gmail.com');
+INSERT INTO BudgetItems (username, category, spending) VALUES ('test', 'Grocery', 100.00);
+INSERT INTO BudgetItems (username, category, spending) VALUES ('test', 'Gas', 1000.00);
+INSERT INTO BudgetItems (username, category, spending) VALUES ('test', 'Other', 900.00);
+INSERT INTO BudgetItems (username, category, spending) VALUES ('test', 'Entertainment', 10.00);
