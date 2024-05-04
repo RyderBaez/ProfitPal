95758392087df3415fd791d50e762cc3b9b77a92
Ryder Baez <rbbaez@usc.edu>
2024-05-03 20:19:29 -0700

    Add files via upload

---
+++ src/main/java/SentData.java
@@ -0,0 +1,52 @@
+
+
+	public class SentData {
+
+	private String grocery;
+	private String restaurant;
+	private String shopping;
+	private String gas;
+	private String code;
+
+	public String getGrocery() {
+	return grocery;
+	}
+
+	public void setGrocery(String grocery) {
+	this.grocery = grocery;
+	}
+
+	public String getRestaurant() {
+	return restaurant;
+	}
+
+	public void setRestaurant(String restaurant) {
+	this.restaurant = restaurant;
+	}
+
+	public String getShopping() {
+	return shopping;
+	}
+
+	public void setShopping(String shopping) {
+	this.shopping = shopping;
+	}
+
+	public String getGas() {
+	return gas;
+	}
+
+	public void setGas(String gas) {
+	this.gas = gas;
+	}
+
+	public String getCode() {
+	return code;
+	}
+
+	public void setCode(String code) {
+	this.code = code;
+	}
+
+	}
+
